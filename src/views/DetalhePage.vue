eb5be227cd75b64b89f1c122843b9a9055c6f9cf
isadora da Maia Torres <isadoradamaiatorres@gmail.com>
2026-05-27 14:43:46 -0300

    git commit -m "feat: criar DetalhePage com param :id e IonBackButton"

---
+++ src/views/DetalhePage.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import { useIonRouter } from "@ionic/vue";
+import { storeToRefs } from "pinia";
+import { useTarefasStore } from "../stores/tarefas";
+const route = useRoute();
+const ionRouter = useIonRouter();
+const store = useTarefasStore();
+const { tarefas } = storeToRefs(store);
+const { concluir } = store;
+const id = computed(() => Number(route.params.id));
+const tarefa = computed(() => tarefas.value.find((t) => t.id === id.value));
+function voltar() {
+  if (ionRouter.canGoBack()) ionRouter.back();
+  else ionRouter.replace("/tabs/tarefas");
+}
+</script>
