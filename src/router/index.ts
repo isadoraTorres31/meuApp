52da527ae5d16b09278e3573357784ed3e71ab12
isadora da Maia Torres <isadoradamaiatorres@gmail.com>
2026-05-27 14:45:53 -0300

    git commit -m "feat: guard beforeEach para /tabs/perfil com meta requerNome"

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import { RouteRecordRaw } from "vue-router";
 import HomePage from "../views/HomePage.vue";
 import TarefasPage from "../views/TarefasPage.vue";
 import LoginPage from "@/views/LoginPage.vue";
+import
 
 const routes: Array<RouteRecordRaw> = [
   { path: "/", redirect: "/tabs/tarefas" },
@@ -29,5 +30,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
+// No final do router/index.ts, antes do export
+router.beforeEach((to, _, next) => {
+ const usuStore = useUsuarioStore()
+ if (to.meta.requerNome && !usuStore.nome) {
+ next('/tabs/tarefas')
+ } else {
+ next()
+ }
+})
 
 export default router;
